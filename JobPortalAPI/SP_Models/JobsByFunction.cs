0019017769b3f2f7bf6f9528f4bb31197e873171
venugopalnambimt <159425550+venugopalnambimt@users.noreply.github.com>
2024-03-07 11:45:36 +0530

    update master repository

--- JobPortalAPI/SP_Models/JobsByFunction.cs
+++ JobPortalAPI/SP_Models/JobsByFunction.cs
@@ -10,6 +10,6 @@ namespace JobPortalAPI.SP_Models
     {
         public string FunctionName {get;set;}
         public int Count {get;set;}
-        public List<Job> Jobs {get;set;}
+        public List<string> JobNames {get;set;}
     }
 }
